03a91aeeeed4efc6961dd64e1cc1cb827c49d0df
StevenEduardoReyesLeon <stevenrl325@gmail.com>
2023-02-17 20:02:16 -0500

    Ciclo For

---
+++ main.js
@@ -0,0 +1,28 @@
+
+alert("Abrir la consola o presione F12")
+
+
+let i = 0;
+let bandera = true;
+for(i; bandera;){
+    console.log(bandera);
+    bandera = (i<5) ? ++i: false;
+}
+
+
+// for (let i = 0; i <=5; i++) {
+//     console.log(i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
